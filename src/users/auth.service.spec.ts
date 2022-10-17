361046c1dc7e96eaad5b493bf9f8e2da38df14a3
skydihy <guylaxy.dev@gmail.com>
2022-10-17 15:57:54 +0700

    setup unit testing

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,32 @@
+import { UsersService } from './users.service';
+import { Test } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { User } from './users.entity';
+
+describe('AuthService', () => {
+  let service: AuthService;
+
+  beforeEach(async () => {
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({
+          id: 1,
+          email,
+          password,
+        } as User),
+    };
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: fakeUsersService },
+      ],
+    }).compile();
+
+    service = module.get(AuthService);
+  });
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+});
