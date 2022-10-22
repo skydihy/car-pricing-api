76c34a046cb4e812d604721cdc924a6a90630bd8
Guysana <guylaxy.dev@gmail.com>
2022-10-22 23:04:29 +0700

    Update: get estimate

--- src/reports/dtos/get-estimate.dto.ts
+++ src/reports/dtos/get-estimate.dto.ts
@@ -1,3 +1,4 @@
+import { Transform } from 'class-transformer';
 import {
   IsLatitude,
   IsLongitude,
@@ -7,33 +8,32 @@ import {
   Min,
 } from 'class-validator';
 
-export class CreateReportDto {
+export class GetEstimateDto {
   @IsString()
   make: string;
 
   @IsString()
   model: string;
 
+  @Transform(({ value }) => parseInt(value))
   @IsNumber()
   @Min(1930)
   @Max(2050)
   year: number;
 
+  @Transform(({ value }) => parseInt(value))
   @IsNumber()
   @Min(0)
   @Max(1000000)
   mileage: number;
 
+  @Transform(({ value }) => parseFloat(value))
   @IsNumber()
   @IsLongitude()
   lng: number;
 
+  @Transform(({ value }) => parseFloat(value))
   @IsNumber()
   @IsLatitude()
   lat: number;
-
-  @IsNumber()
-  @Min(0)
-  @Max(1000000)
-  price: number;
 }
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,9 +2,11 @@ import { ReportsService } from './reports.service';
 import {
   Body,
   Controller,
+  Get,
   Param,
   Patch,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
@@ -16,10 +18,16 @@ import { Serailize } from '../interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { AdminGuard } from '../guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    console.log(query)
+  }
 
   @Post()
   @UseGuards(AuthGuard)
